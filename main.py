96c36ca31fde3630d9f4ed1af6f3c9e34efba929
ChehaYuri <sacra1364069@gmail.com>
2023-04-03 09:43:45 +0300

    задание 4 - нахождение макс длины слова в тексте

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-# import pandas as pd
-# df = pd.read_csv('famaly.csv'   )
-# print(df)
+import pandas as pd
+df = pd.read_csv('famaly.csv'   )
+print(df)
 
-# name = input("Введите имя: ")
-# surname = input("Введите фамилию: ")
-# age = input("Введите возраст: ")
-# with open("user_data.txt", "w", -1, 'utf-8') as file:
-#     file.write("Имя: " + name + "\n")
-#     file.write("Фамилия: " + surname + "\n")
-#     file.write("Возраст: " + age + "\n")
-# print("Ok")
+name = input("Введите имя: ")
+surname = input("Введите фамилию: ")
+age = input("Введите возраст: ")
+with open("user_data.txt", "w", -1, 'utf-8') as file:
+    file.write("Имя: " + name + "\n")
+    file.write("Фамилия: " + surname + "\n")
+    file.write("Возраст: " + age + "\n")
+print("Ok")
 
-# lines = int(input('Введите число: '))
-#
-# if lines <= 0:
-#     print('Количество строк должно быть положительным целым числом')
-# else:
-#     with open('famaly.csv', 'r') as f:
-#         f_lines = f.readlines()
-#         print('Последние', lines, 'строк файла', ':')
-#         for line in f_lines[-lines:]:
-#             print(line.strip())
+lines = int(input('Введите число: '))
+
+if lines <= 0:
+    print('Количество строк должно быть положительным целым числом')
+else:
+    with open('famaly.csv', 'r') as f:
+        f_lines = f.readlines()
+        print('Последние', lines, 'строк файла', ':')
+        for line in f_lines[-lines:]:
+            print(line.strip())
 
 
 str_ = input("Введите строку: ")
@@ -33,3 +33,4 @@ for word in str_.split():
     if len(word) == max_len:
         str_2.append(word)
 print(str_2)
+
