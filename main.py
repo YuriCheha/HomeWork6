9178957046c3ccebe4e0d44c0c2d37486e29f97b
ChehaYuri <sacra1364069@gmail.com>
2023-04-03 09:22:16 +0300

    задание 4 - нахождение макс длины слова в тексте

--- main.py
+++ main.py
@@ -11,13 +11,25 @@
 #     file.write("Возраст: " + age + "\n")
 # print("Ok")
 
-lines = int(input('Введите число: '))
+# lines = int(input('Введите число: '))
+#
+# if lines <= 0:
+#     print('Количество строк должно быть положительным целым числом')
+# else:
+#     with open('famaly.csv', 'r') as f:
+#         f_lines = f.readlines()
+#         print('Последние', lines, 'строк файла', ':')
+#         for line in f_lines[-lines:]:
+#             print(line.strip())
 
-if lines <= 0:
-    print('Количество строк должно быть положительным целым числом')
-else:
-    with open('famaly.csv', 'r') as f:
-        f_lines = f.readlines()
-        print('Последние', lines, 'строк файла', ':')
-        for line in f_lines[-lines:]:
-            print(line.strip())
+
+str_ = input("Введите строку: ")
+max_len = 0
+for word in str_.split():
+    if len(word) > max_len:
+        max_len = len(word)
+str_2 = []
+for word in str_.split():
+    if len(word) == max_len:
+        str_2.append(word)
+print(str_2)
