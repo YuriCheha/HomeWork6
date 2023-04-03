0c84cffdea5322d120994184d873523084abd4aa
ChehaYuri <sacra1364069@gmail.com>
2023-04-03 13:16:28 +0300

    добавил 2 задаич

--- main.py
+++ main.py
@@ -34,3 +34,14 @@ for word in str_.split():
         str_2.append(word)
 print(str_2)
 
+import pandas as pd
+df = pd.read_csv('famaly.csv')
+json_data = df.to_json(orient='records')
+with open('famaly.json', 'w') as f:
+    f.write(json_data)
+
+file = input("Введите имя файла: ")
+text = input("Введите текст: ").upper()
+
+with open('file.txt', 'w', -1, 'UTF-8') as f:
+    f.write(text)
