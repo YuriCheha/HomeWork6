827c19c53ee1dc1cc608046c01c0b68328cb0972
ChehaYuri <sacra1364069@gmail.com>
2023-04-03 09:04:55 +0300

    задание 2 - создание файла имя фамилия восраст

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import pandas as pd
-df = pd.read_csv('famaly.csv'   )
-print(df)
+# import pandas as pd
+# df = pd.read_csv('famaly.csv'   )
+# print(df)
+
+name = input("Введите имя: ")
+surname = input("Введите фамилию: ")
+age = input("Введите возраст: ")
+with open("user_data.txt", "w") as file:
+    file.write("Имя: " + name + "\n")
+    file.write("Фамилия: " + surname + "\n")
+    file.write("Возраст: " + age + "\n")
+print("file")
