67e1359a16cc805c3338eef86714314fce6f3a61
ChehaYuri <sacra1364069@gmail.com>
2023-04-03 13:13:59 +0300

    comm

--- main2.py
+++ main2.py
@@ -1,11 +1,11 @@
-# import pandas as pd
-# df = pd.read_csv('famaly.csv')
-# json_data = df.to_json(orient='records')
-# with open('famaly.json', 'w') as f:
-#     f.write(json_data)
-#
-# file = input("Введите имя файла: ")
-# text = input("Введите текст: ").upper()
-#
-# with open('file.txt', 'w', -1, 'UTF-8') as f:
-#     f.write(text)
+import pandas as pd
+df = pd.read_csv('famaly.csv')
+json_data = df.to_json(orient='records')
+with open('famaly.json', 'w') as f:
+    f.write(json_data)
+
+file = input("Введите имя файла: ")
+text = input("Введите текст: ").upper()
+
+with open('file.txt', 'w', -1, 'UTF-8') as f:
+    f.write(text)
