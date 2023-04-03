b98af742f36de7e94588356650f648fedbcbe68d
ChehaYuri <sacra1364069@gmail.com>
2023-04-03 10:03:03 +0300

    задание 5 перед csv в json

--- main2.py
+++ main2.py
@@ -4,8 +4,8 @@ json_data = df.to_json(orient='records')
 with open('famaly.json', 'w') as f:
     f.write(json_data)
 
-# file = input("Введите имя файла: ")
-# text = input("Введите текст: ").upper()
-#
-# with open('file.txt', 'w', -1, 'UTF-8') as f:
-#     f.write(text)
+file = input("Введите имя файла: ")
+text = input("Введите текст: ").upper()
+
+with open('file.txt', 'w', -1, 'UTF-8') as f:
+    f.write(text)
